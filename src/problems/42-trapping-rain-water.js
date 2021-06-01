5083e7dfa1641bb0c83efc02b441041defaa0703
MaginaCheng <magina.cheng@i9i8.com>
2021-06-01 20:13:36 +0800

    43

--- src/problems/42-trapping-rain-water.js
+++ src/problems/42-trapping-rain-water.js
@@ -114,21 +114,304 @@ export var trap3 = function (height) {
 
 export var trap4 = function (height) {
   const len = height.length
-  let left = 0
-  let right = []
-  right[len - 1] = 0
   let sum = 0
-  for (let i = len - 2; i >= 1; i--) {
-    right[i] = Math.max(height[i + 1], right[i + 1])
-  }
+
+  let left = 0
+  let leftIndex = 1
+  let right = 0
+  let rightIndex = len - 2
   for (let i = 1; i <= len - 2; i++) {
-    left = Math.max(height[i - 1], left)
-    let deep = Math.min(left, right[i])
-    if (deep > height[i]) {
-      sum += deep - height[i]
+    left = Math.max(left, height[leftIndex - 1])
+    right = Math.max(right, height[rightIndex + 1])
+    if (height[leftIndex - 1] <= height[rightIndex + 1]) {
+      if (height[leftIndex] < left) {
+        sum += left - height[leftIndex]
+      }
+      leftIndex++
+    } else if (height[leftIndex - 1] > height[rightIndex + 1]) {
+      if (height[rightIndex] < right) {
+        sum += right - height[rightIndex]
+      }
+      rightIndex--
     }
   }
   return sum
 }
 
-// trap2([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
+export var trap6 = function (height) {
+  const getData = (heigt, a, b, start) => {
+    let i = 1
+    let count = 0
+    while (a + i < b) {
+      count += start - heigt[a + i]
+      i++
+    }
+    return count
+  }
+  let leftIndex = -1
+  const len = height.length
+  // const list = []
+  let count = 0
+  let cache = []
+  for (let i = 0; i < len; i++) {
+    if (height[i]) {
+      if (leftIndex === -1 || (leftIndex !== -1 && height[i] >= height[leftIndex])) {
+        leftIndex = i
+        cache.push(i)
+        if (cache.length === 2) {
+          if (cache[1] - cache[0] > 1) {
+            // list.push(cache)
+            count += getData(height, cache[0], cache[1], height[cache[0]])
+          }
+          if (i < len - 2) {
+            cache = [i]
+          } else {
+            cache = []
+          }
+        }
+      }
+    }
+  }
+  // console.log(list)
+  if (cache.length) {
+    cache = []
+    let rightIndex = -1
+    for (let i = len - 1; i >= leftIndex; i--) {
+      if (height[i]) {
+        if (rightIndex === -1 || (rightIndex !== -1 && height[i] >= height[rightIndex])) {
+          rightIndex = i
+          cache.unshift(i)
+          if (cache.length === 2) {
+            if (cache[1] - cache[0] > 1) {
+              // list.push(cache)
+              count += getData(height, cache[0], cache[1], height[cache[1]])
+            }
+            cache = [i]
+          }
+        }
+      }
+    }
+  }
+  // console.log(list)
+
+  // console.log(count)
+  return count
+}
+
+export var trap5 = function (height) {
+  let leftIndex = -1
+  const len = height.length
+  let count = 0
+  let waitIndex = -1
+  for (let i = 0; i < len; i++) {
+    if (height[i]) {
+      if (leftIndex === -1 || (leftIndex !== -1 && height[i] >= height[leftIndex])) {
+        leftIndex = i
+        if (waitIndex === -1) {
+          waitIndex = i
+        } else {
+          if (i - waitIndex > 1) {
+            count += getData(height, waitIndex, i, height[waitIndex])
+          }
+          if (i < len - 2) {
+            waitIndex = i
+          } else {
+            waitIndex = -1
+          }
+        }
+      }
+    }
+  }
+  if (waitIndex != -1) {
+    waitIndex = -1
+    let rightIndex = -1
+    for (let i = len - 1; i >= leftIndex; i--) {
+      if (height[i]) {
+        if (rightIndex === -1 || (rightIndex !== -1 && height[i] >= height[rightIndex])) {
+          rightIndex = i
+          if (waitIndex === -1) {
+            waitIndex = i
+          } else {
+            if (waitIndex - i > 1) {
+              count += getData(height, i, waitIndex, height[waitIndex])
+            }
+            waitIndex = i
+          }
+        }
+      }
+    }
+  }
+  return count
+}
+
+// trap6([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
+// trap5([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
+// trap6([4, 2, 3])
+// trap5([4, 2, 3])
+
+// console.log(1)
+
+export var trap7 = function (height) {
+  let leftIndex = -1
+  const len = height.length
+  // const list = []
+  let count = 0
+  let cache = []
+  for (let i = 0; i < len; i++) {
+    if (height[i]) {
+      if (leftIndex === -1 || (leftIndex !== -1 && height[i] >= height[leftIndex])) {
+        leftIndex = i
+        cache.push(i)
+        if (cache.length === 2) {
+          if (cache[1] - cache[0] > 1) {
+            // list.push(cache)
+            count += getData(height, cache[0], cache[1], height[cache[0]])
+          }
+          if (i < len - 2) {
+            cache = [i]
+          } else {
+            cache = []
+          }
+        }
+      }
+    }
+  }
+  // console.log(list)
+  if (cache.length) {
+    cache = []
+    let rightIndex = -1
+    for (let i = len - 1; i >= leftIndex; i--) {
+      if (height[i]) {
+        if (rightIndex === -1 || (rightIndex !== -1 && height[i] >= height[rightIndex])) {
+          rightIndex = i
+          cache.unshift(i)
+          if (cache.length === 2) {
+            if (cache[1] - cache[0] > 1) {
+              // list.push(cache)
+              count += getData(height, cache[0], cache[1], height[cache[1]])
+            }
+            cache = [i]
+          }
+        }
+      }
+    }
+  }
+  function getData(heigt, a, b, start) {
+    let i = 1
+    let count = 0
+    while (a + i < b) {
+      count += start - heigt[a + i]
+      i++
+    }
+
+    // console.log(count)
+    return count
+  }
+  return count
+}
+
+export var trap8 = function (height) {
+  let leftIndex = -1
+  const len = height.length
+  let count = 0
+  let waitIndex = -1
+  for (let i = 0; i < len; i++) {
+    if (height[i]) {
+      if (height[i] >= height[leftIndex] || leftIndex === -1) {
+        // if (leftIndex === -1 || (leftIndex !== -1 && height[i] >= height[leftIndex])) {
+        leftIndex = i
+        if (waitIndex === -1) {
+          waitIndex = i
+        } else {
+          if (i - waitIndex > 1) {
+            count += getData(height, waitIndex, i, height[waitIndex])
+          }
+          if (i < len - 2) {
+            waitIndex = i
+          } else {
+            waitIndex = -1
+          }
+        }
+      }
+    }
+  }
+  if (waitIndex != -1) {
+    waitIndex = -1
+    let rightIndex = -1
+    for (let i = len - 1; i >= leftIndex; i--) {
+      if (height[i]) {
+        if (height[i] >= height[rightIndex] || rightIndex === -1) {
+          //  if (rightIndex === -1 || (rightIndex !== -1 && height[i] >= height[rightIndex])) {
+          rightIndex = i
+          if (waitIndex === -1) {
+            waitIndex = i
+          } else {
+            if (waitIndex - i > 1) {
+              count += getData(height, i, waitIndex, height[waitIndex])
+            }
+            waitIndex = i
+          }
+        }
+      }
+    }
+  }
+  return count
+}
+
+export var trap9 = function (height) {
+  let leftIndex = -1
+  const len = height.length
+  let count = 0
+  let waitIndex = -1
+  for (let i = 0; i < len; i++) {
+    if (height[i]) {
+      if (leftIndex === -1 || (leftIndex !== -1 && height[i] >= height[leftIndex])) {
+        leftIndex = i
+        if (waitIndex === -1) {
+          waitIndex = i
+        } else {
+          if (i - waitIndex > 1) {
+            count += getData(height, waitIndex, i, height[waitIndex])
+          }
+          if (i < len - 2) {
+            waitIndex = i
+          } else {
+            waitIndex = -1
+          }
+        }
+      }
+    }
+  }
+  if (waitIndex != -1) {
+    waitIndex = -1
+    let rightIndex = -1
+    for (let i = len - 1; i >= leftIndex; i--) {
+      if (height[i]) {
+        if (rightIndex === -1 || (rightIndex !== -1 && height[i] >= height[rightIndex])) {
+          rightIndex = i
+          if (waitIndex === -1) {
+            waitIndex = i
+          } else {
+            if (waitIndex - i > 1) {
+              count += getData(height, i, waitIndex, height[waitIndex])
+            }
+            waitIndex = i
+          }
+        }
+      }
+    }
+  }
+
+  function getData(heigt, a, b, start) {
+    let i = 1
+    let count = 0
+    while (a + i < b) {
+      count += start - heigt[a + i]
+      i++
+    }
+
+    // console.log(count)
+    return count
+  }
+  return count
+}
--- src/problems/42-trapping-rain-water.test.js
+++ src/problems/42-trapping-rain-water.test.js
@@ -1,4 +1,4 @@
-import { trap, trap2, trap3 } from './42-trapping-rain-water'
+import { trap, trap2, trap3, trap4, trap5, trap6, trap7, trap8, trap9 } from './42-trapping-rain-water'
 
 const list = [
   [[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6],
@@ -7,34 +7,125 @@ const list = [
   [[0, 2, 0], 0]
 ]
 
-describe('42-trapping-rain-water trap', () => {
-  for (let i = 0; i < list.length; i++) {
-    it('item ==> ' + (i + 1), () => {
-      expect(trap(list[i][0])).toBe(list[i][1])
-    })
-  }
-})
+// describe('42-trapping-rain-water trap', () => {
+//   for (let i = 0; i < list.length; i++) {
+//     it('item ==> ' + (i + 1), () => {
+//       expect(trap(list[i][0])).toBe(list[i][1])
+//     })
+//   }
+// })
 
-describe('42-trapping-rain-water trap2', () => {
-  for (let i = 0; i < list.length; i++) {
-    it('item ==> ' + (i + 1), () => {
-      expect(trap2(list[i][0])).toBe(list[i][1])
-    })
-  }
-})
+// describe('42-trapping-rain-water trap2', () => {
+//   for (let i = 0; i < list.length; i++) {
+//     it('item ==> ' + (i + 1), () => {
+//       expect(trap2(list[i][0])).toBe(list[i][1])
+//     })
+//   }
+// })
 
-describe('42-trapping-rain-water trap3', () => {
-  for (let i = 0; i < list.length; i++) {
-    it('item ==> ' + (i + 1), () => {
-      expect(trap3(list[i][0])).toBe(list[i][1])
-    })
-  }
-})
+// describe('42-trapping-rain-water trap3', () => {
+//   for (let i = 0; i < list.length; i++) {
+//     it('item ==> ' + (i + 1), () => {
+//       expect(trap3(list[i][0])).toBe(list[i][1])
+//     })
+//   }
+// })
 
-describe('trap3 all', () => {
-  ;[trap, trap2, trap3].forEach((fn, index) => {
+// describe('42-trapping-rain-water trap4', () => {
+//   for (let i = 0; i < list.length; i++) {
+//     it('item ==> ' + (i + 1), () => {
+//       expect(trap4(list[i][0])).toBe(list[i][1])
+//     })
+//   }
+// })
+
+// describe('42-trapping-rain-water trap5', () => {
+//   for (let i = 0; i < list.length; i++) {
+//     it('item ==> ' + (i + 1), () => {
+//       expect(trap5(list[i][0])).toBe(list[i][1])
+//     })
+//   }
+// })
+
+// describe('42-trapping-rain-water trap6', () => {
+//   for (let i = 0; i < list.length; i++) {
+//     it('item ==> ' + (i + 1), () => {
+//       expect(trap6(list[i][0])).toBe(list[i][1])
+//     })
+//   }
+// })
+
+// describe('trap3 all', () => {
+//   ;[trap, trap2, trap3, trap4, trap5, trap6, trap7].forEach((fn, index) => {
+//     it('item ==> ' + (index + 1), () => {
+//       for (let j = 0; j < 50000; j++) {
+//         for (let i = 0; i < list.length; i++) {
+//           fn(list[i][0])
+//         }
+//       }
+//       expect(1).toBe(1)
+//     })
+//   })
+//   // √ item ==> 1 (59 ms) // 逐个遍历 o(2n)=>o(2n) o(n)
+//   // √ item ==> 2 (374 ms) // 逐行 o(n) o(n)
+//   // √ item ==> 3 (364 ms) // 单指针 o(n) o(n)
+//   // √ item ==> 4 (224 ms) // 双指针 o(n) o(1)
+//   // √ item ==> 5 (11 ms) // 逐个遍历 o(2n)=>o(n) o(1)
+//   // √ item ==> 6 (50 ms) // 对比1内置了功能函数
+//   // √ item ==> 7 (49 ms)
+
+//   // √ item ==> 1 (62 ms)
+//   // √ item ==> 2 (362 ms)
+//   // √ item ==> 3 (349 ms)
+//   // √ item ==> 4 (223 ms)
+//   // √ item ==> 5 (10 ms)
+//   // √ item ==> 6 (51 ms)
+//   // √ item ==> 7 (56 ms)
+
+//   // √ item ==> 1 (60 ms)
+//   // √ item ==> 2 (361 ms)
+//   // √ item ==> 3 (339 ms)
+//   // √ item ==> 4 (219 ms)
+//   // √ item ==> 5 (11 ms)
+//   // √ item ==> 6 (50 ms)
+//   // √ item ==> 7 (51 ms)
+// })
+
+// describe('42-trapping-rain-water trap8', () => {
+//   for (let i = 0; i < list.length; i++) {
+//     it('item ==> ' + (i + 1), () => {
+//       expect(trap8(list[i][0])).toBe(list[i][1])
+//     })
+//   }
+// })
+
+// describe('trap-self all', () => {
+//   ;[trap5, trap8].forEach((fn, index) => {
+//     it('item ==> ' + (index + 1), () => {
+//       for (let j = 0; j < 50000; j++) {
+//         for (let i = 0; i < list.length; i++) {
+//           fn(list[i][0])
+//         }
+//       }
+//       expect(1).toBe(1)
+//     })
+//   })
+//   // √ item ==> 1 (15 ms)
+//   // √ item ==> 2 (71 ms)
+// })
+
+// describe('42-trapping-rain-water trap9', () => {
+//   for (let i = 0; i < list.length; i++) {
+//     it('item ==> ' + (i + 1), () => {
+//       expect(trap9(list[i][0])).toBe(list[i][1])
+//     })
+//   }
+// })
+
+describe('trap5-trap9 fun in out', () => {
+  ;[trap9, trap5].forEach((fn, index) => {
     it('item ==> ' + (index + 1), () => {
-      for (let j = 0; j < 10000; j++) {
+      for (let j = 0; j < 50000; j++) {
         for (let i = 0; i < list.length; i++) {
           fn(list[i][0])
         }
